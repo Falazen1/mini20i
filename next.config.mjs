d5170675648702e400a78a9f4eff87ff9880065d
Falazen1 <zalandaria@gmail.com>
2025-05-02 14:09:14 -0700

    Ribbit Ribbit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-// next.config.mjs
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
@@ -6,6 +5,7 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
