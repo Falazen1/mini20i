8e7e54e83db6dc5186e3df913c1a77fa67eeb99e
Falazen1 <zalandaria@gmail.com>
2025-05-03 19:11:23 -0700

    Ribbit Ribbit

--- app/page.tsx
+++ app/page.tsx
@@ -459,11 +459,12 @@ export default function Page() {
 >
   <div className="w-full aspect-square relative">
   <Image
-  src={token.buyImage || "/default-image.jpg"}
+  src={`/${token.buyImage}`}
   alt={`${token.name} buy more`}
   fill
   className="object-contain opacity-60 hover:opacity-80 transition"
 />
+
 
   </div>
 </div>
