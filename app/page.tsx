9c4a048e298994bdcc006b3cec74e965256f2ad0
Falazen1 <zalandaria@gmail.com>
2025-05-03 20:50:20 -0700

    Ribbit Ribbit

--- app/page.tsx
+++ app/page.tsx
@@ -415,9 +415,6 @@ export default function Page() {
     </div>
   )}
 </div>
-{activeFilter !== "all" && (
-  <p className="text-sm text-gray-500 mt-1">{token.about}</p>
-)}
 
 <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-4">
   {(inscriptions[token.key] || []).map((inscription) => {
