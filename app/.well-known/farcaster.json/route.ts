01b23ca7b1dea93068c55a551ca3a50776937244
Falazen1 <zalandaria@gmail.com>
2025-05-04 16:56:49 -0700

    Ribbit Ribbit

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,20 +1,20 @@
 export async function GET() {
   return Response.json({
-  accountAssociation: {
-    header: "eyJmaWQiOjUxMjY4NywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDk2OTg0NDBGZjliMjVFNzkxMThBQzdkMTU3NDdGMEI3RDZBODc1MTQifQ",
-    payload: "eyJkb21haW4iOiJtaW5pMjBpLnZlcmNlbC5hcHAifQ",
-    signature: "MHhjNjNlZjI5NDVjMjJhMzE5OGZmZjIwZWNhNDhlZWI5NWFiZGRiMjViMTUwMWJjM2MzODBiODM5MjlmZjdhZWJjM2Y1OWM3NTI3NTZkMTljYWM4ZDQ0ZDQ0NjU3NTVmYWI0YTllODk1N2IwMDg4ZmVjZGZkY2RkMWYzMDdmNDM5NTFi",
+  "accountAssociation": {
+    "header": "eyJmaWQiOjUxMjY4NywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDk2OTg0NDBGZjliMjVFNzkxMThBQzdkMTU3NDdGMEI3RDZBODc1MTQifQ",
+    "payload": "eyJkb21haW4iOiJtaW5pMjBpLnZlcmNlbC5hcHAifQ",
+    "signature": "MHhjNjNlZjI5NDVjMjJhMzE5OGZmZjIwZWNhNDhlZWI5NWFiZGRiMjViMTUwMWJjM2MzODBiODM5MjlmZjdhZWJjM2Y1OWM3NTI3NTZkMTljYWM4ZDQ0ZDQ0NjU3NTVmYWI0YTllODk1N2IwMDg4ZmVjZGZkY2RkMWYzMDdmNDM5NTFi",
   },
   frame: {
     version: "next",
     name: "Mini20i",
-    homeUrl: "mini20i.vercel.app",
-    iconUrl: "raw.githubusercontent.com/Falazen1/Inscription_Viewer/refs/heads/main/ERC20i%20ecosystem.jpg",
-    imageUrl: "raw.githubusercontent.com/Falazen1/Inscription_Viewer/refs/heads/main/ERC20i%20ecosystem.jpg",
+    homeUrl: "https://mini20i.vercel.app",
+    iconUrl: "https://raw.githubusercontent.com/Falazen1/Inscription_Viewer/refs/heads/main/ERC20i%20ecosystem.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Falazen1/Inscription_Viewer/refs/heads/main/ERC20i%20ecosystem.jpg",
     buttonTitle: "Launch Mini20i",
-    splashImageUrl: "raw.githubusercontent.com/Falazen1/Inscription_Viewer/refs/heads/main/ERC20i%20ecosystem.jpg",
+    splashImageUrl: "https://raw.githubusercontent.com/Falazen1/Inscription_Viewer/refs/heads/main/ERC20i%20ecosystem.jpg",
     splashBackgroundColor: "#282c34",
-    webhookUrl: "mini20i.vercel.app/api/webhook"
+    webhookUrl: "https://mini20i.vercel.app/api/webhook"
   }
 });
 }
