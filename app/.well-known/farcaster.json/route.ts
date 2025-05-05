f48e06c65b06103dab19a51c0e7649a6a59e3ce5
Falazen1 <zalandaria@gmail.com>
2025-05-04 18:46:06 -0700

    Ribbit Ribbit

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -5,15 +5,15 @@ export async function GET() {
       "payload": "eyJkb21haW4iOiJtaW5pMjBpLnZlcmNlbC5hcHAifQ",
       "signature": "MHhjNjNlZjI5NDVjMjJhMzE5OGZmZjIwZWNhNDhlZWI5NWFiZGRiMjViMTUwMWJjM2MzODBiODM5MjlmZjdhZWJjM2Y1OWM3NTI3NTZkMTljYWM4ZDQ0ZDQ0NjU3NTVmYWI0YTllODk1N2IwMDg4ZmVjZGZkY2RkMWYzMDdmNDM5NTFi",
     },
-    frame: {
-      version: "next",
-      imageUrl: "https://raw.githubusercontent.com/Falazen1/Inscription_Viewer/refs/heads/main/ERC20i%20ecosystem.jpg",
-      button: {
-        title: "Launch Mini20i",
-        action: {
-          type: "launch_frame",
-          name: "mini20i",
-          url: "https://mini20i.vercel.app",
+    "frame": {
+      "version": "next",
+      "imageUrl": "https://raw.githubusercontent.com/Falazen1/Inscription_Viewer/refs/heads/main/ERC20i%20ecosystem.jpg",
+      "button": {
+        "title": "Launch Mini20i",
+        "action": {
+          "type": "launch_frame",
+          "name": "mini20i",
+          "url": "https://mini20i.vercel.app",
         }
       }
     }
