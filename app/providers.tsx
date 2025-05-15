a11c77e5f89a92526be17dc7e9cb9320bec2be78
Falazen1 <zalandaria@gmail.com>
2025-05-15 14:00:50 -0700

    Ribbit Ribbit

--- app/providers.tsx
+++ app/providers.tsx
@@ -11,24 +11,32 @@ import { base } from 'viem/chains';
 const queryClient = new QueryClient();
 
 export function Providers({ children }: PropsWithChildren) {
+  const isWarpcast =
+    typeof window !== "undefined" &&
+    typeof navigator !== "undefined" &&
+    navigator.userAgent.toLowerCase().includes("warpcast");
+
   return (
-    
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider
-          apiKey="3KA49gYhtfR0hrw5L7L0nPVYlO1z4tyE"
-          chain={base}
-          config={{
-            appearance: {
-              name: "mini20i",
-              logo: "https://mini-20i.app/logo.png",
-              theme: "default",
-              mode: "auto",
-            },
-          }}
-        >
+        {isWarpcast ? (
           <MiniKitProvider>{children}</MiniKitProvider>
-        </OnchainKitProvider>
+        ) : (
+          <OnchainKitProvider
+            apiKey="3KA49gYhtfR0hrw5L7L0nPVYlO1z4tyE"
+            chain={base}
+            config={{
+              appearance: {
+                name: "mini20i",
+                logo: "https://mini-20i.app/logo.png",
+                theme: "default",
+                mode: "auto",
+              },
+            }}
+          >
+            {children}
+          </OnchainKitProvider>
+        )}
       </QueryClientProvider>
     </WagmiProvider>
   );
