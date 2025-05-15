de73ac0488498d2638c962c43967c774abcd5734
Falazen1 <zalandaria@gmail.com>
2025-05-15 14:28:41 -0700

    Ribbit Ribbit

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,30 +5,50 @@ import { config } from './helpers/wagmiConfig';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { MiniKitProvider } from './providers/MiniKitProvider';
+import { MiniKitProvider as MKProvider } from '@coinbase/onchainkit/minikit';
 import { base } from 'viem/chains';
 
 const queryClient = new QueryClient();
-
 export function Providers({ children }: PropsWithChildren) {
+  const isWarpcast =
+    typeof window !== "undefined" &&
+    typeof navigator !== "undefined" &&
+    navigator.userAgent.toLowerCase().includes("warpcast");
+
   return (
-    
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider
-          apiKey="3KA49gYhtfR0hrw5L7L0nPVYlO1z4tyE"
-          chain={base}
-          config={{
-            appearance: {
-              name: "mini20i",
-              logo: "https://mini-20i.app/logo.png",
-              theme: "default",
-              mode: "auto",
-            },
-          }}
-        >
-          <MiniKitProvider>{children}</MiniKitProvider>
-        </OnchainKitProvider>
+        {isWarpcast ? (
+          <MKProvider
+            apiKey="3KA49gYhtfR0hrw5L7L0nPVYlO1z4tyE"
+            chain={base}
+            config={{
+              appearance: {
+                name: "mini20i",
+                logo: "https://mini-20i.app/logo.png",
+                theme: "default",
+                mode: "auto",
+              },
+            }}
+          >
+            {children}
+          </MKProvider>
+        ) : (
+          <OnchainKitProvider
+            apiKey="3KA49gYhtfR0hrw5L7L0nPVYlO1z4tyE"
+            chain={base}
+            config={{
+              appearance: {
+                name: "mini20i",
+                logo: "https://mini-20i.app/logo.png",
+                theme: "default",
+                mode: "auto",
+              },
+            }}
+          >
+            {children}
+          </OnchainKitProvider>
+        )}
       </QueryClientProvider>
     </WagmiProvider>
   );
